41fa634d2a8081472766f821523d9b45695141d9
Dantas-Lab <bields5@gmail.com>
2026-07-05 14:46:33 -0300

    test: ensure Appium session cleanup

--- tests/smoke/test_android_connection.py
+++ tests/smoke/test_android_connection.py
@@ -4,6 +4,7 @@ from driver.driver_factory import create_android_driver
 def test_android_connection():
     driver = create_android_driver()
 
-    assert driver.session_id is not None
-
-    driver.quit()
+    try:
+        assert driver.session_id is not None
+    finally:
+        driver.quit()
